7afada091ca3c52c38618b56e4cb374bd71e762e
yatharth1cr <yatharthgiri187@gmail.com>
2023-08-11 13:58:47 +0530

    added extension folder

---
+++ todo_app/extension/popup.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PopupApp from './PopupApp'; // Import your React component
+
+const popupRoot = document.getElementById('popup-root');
+ReactDOM.render(<PopupApp />, popupRoot);
